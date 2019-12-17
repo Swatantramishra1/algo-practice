5113f45f39727109c52878f1477c89e8bde22930
Swatantra <swatantra.mishra@suprdaily.com>
2019-12-17 16:10:18 +0530

    done with --- longest_common_prefixes with devide and conqure

--- String/longest_common_prefix/longest_common_prefixes_Swatantra.js
+++ String/longest_common_prefix/longest_common_prefixes_Swatantra.js
@@ -22,7 +22,9 @@ function getCommonPrefix(arr) {
   const minSearchLength = Math.min(arr[0].length, arr[length - 1].length);
   const first = arr[0];
   const last = arr[length - 1];
+
   let i = 0;
+
   while (i < minSearchLength && first[i] == last[i]) {
     i++;
   }
@@ -30,6 +32,41 @@ function getCommonPrefix(arr) {
   return first.slice(0, i);
 }
 
-console.log(getCommonPrefix(input));
+// console.log(getCommonPrefix(input));
 
 // Complexity for this problem >>> O(MinSearchLength * n * logn)
+
+//***************** Method 2 --- Devide and Conqure **********************
+
+function commonPrefixUtil(str1, str2) {
+  let result = [];
+  let l1 = str1.length;
+  let l2 = str2.length;
+
+  for (let i = 0, j = 0; i <= l1 - 1 && j <= l2 - 1; i++, j++) {
+    if (str1[i] != str2[j]) break;
+    result.push(str1[i]);
+  }
+
+  return result;
+}
+
+function commonPrefix(arr, low, high) {
+  if (low === high) return arr[low];
+
+  if (high > low) {
+    let mid = Math.floor(low + (high - low) / 2);
+    let str1 = commonPrefix(arr, low, mid);
+    let str2 = commonPrefix(arr, mid + 1, high);
+
+    return commonPrefixUtil(str1, str2);
+  }
+}
+
+const ans = commonPrefix(input, 0, 2);
+
+if (ans.length) {
+  console.log("Longest Common Prefix is = ", ans.join(""));
+} else {
+  console.log("There No Longest Common Prefix");
+}
